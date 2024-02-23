b9586eab909baef0feb5c283919af130de4f74d9
Jucelio coelho <juceliocoelho2022@gmail.com>
2024-02-23 14:36:54 -0300

    Initial commit

---
+++ src/main/java/com/jucelioDev/paymentsistem/PaymentSistemApplication.java
@@ -0,0 +1,13 @@
+package com.jucelioDev.paymentsistem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSistemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentSistemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jucelioDev/paymentsistem/PaymentSistemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jucelioDev.paymentsistem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSistemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
